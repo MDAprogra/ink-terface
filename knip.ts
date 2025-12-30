62a88afa88f1d4180a65847926ed48fbd49509ca
MatthiasInterfas <matthias@interfas.fr>
2025-12-30 17:08:26 +0100

    install knip, create knip.ts to configurate it and execute command

---
+++ knip.ts
@@ -0,0 +1,29 @@
+import type { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  // 1. Points d'entrée
+  entry: [
+    'src/app/**/*.{ts,tsx}',
+    'src/actions/**/*.ts',
+    // J'ai retiré prisma/seed.ts car le "Hint" disait qu'il était redondant (déjà détecté)
+  ],
+
+  // 2. Projet
+  project: ['src/**/*.{ts,tsx}'],
+
+  // 3. Ignorer (C'est ici qu'on corrige le tir !)
+  ignore: [
+    'src/components/ui/**', // Shadcn
+    'src/generated/**', // 👈 AJOUT CRUCIAL : On ignore totalement Prisma généré
+  ],
+
+  // 4. Dépendances à ignorer
+  ignoreDependencies: [
+    'tw-animate-css',
+    'tailwindcss',
+    '@faker-js/faker', // On l'ignore car utilisé seulement dans le seed (qui est hors src/app)
+    'postcss', // On l'ignore pour éviter l'erreur "Unlisted"
+  ],
+};
+
+export default config;
