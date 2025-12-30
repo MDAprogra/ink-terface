82cc9851ab13d278626c53af37a50d1e962dbc81
MatthiasInterfas <matthias@interfas.fr>
2025-12-30 11:57:51 +0100

    addition: animate tabs

--- src/features/Settings/SettingsPage.tsx
+++ src/features/Settings/SettingsPage.tsx
@@ -3,20 +3,30 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ItemTypeComponent } from './Contents/ItemTypeComponent';
 
 export default function SettingsPage() {
+  // const contentAnimation =
+  //   'data-[state=active]:animate-in data-[state=active]:fade-in data-[state=active]:slide-in-from-bottom-2 data-[state=active]:duration-500';
+  const contentAnimation =
+    'data-[state=active]:animate-in data-[state=active]:fade-in-0 data-[state=active]:slide-in-from-left-8 data-[state=active]:duration-500 data-[state=active]:ease-out';
   return (
-    <Tabs defaultValue="ItemType" className="w-100">
-      <TabsList>
+    <Tabs defaultValue="ItemType" className="w-full">
+      <TabsList className="grid w-full grid-cols-4">
         <TabsTrigger value="ItemType">Type de produit</TabsTrigger>
         <TabsTrigger value="MovementType">Type de mouvement</TabsTrigger>
         <TabsTrigger value="Unit">Unité</TabsTrigger>
         <TabsTrigger value="Supplier">Fournisseur</TabsTrigger>
       </TabsList>
-      <TabsContent value="ItemType">
+      <TabsContent value="ItemType" className={contentAnimation}>
         <ItemTypeComponent />
       </TabsContent>
-      <TabsContent value="MovementType">Change your password here.</TabsContent>
-      <TabsContent value="Unit">Make changes to your account here.</TabsContent>
-      <TabsContent value="Supplier">Change your password here.</TabsContent>
+      <TabsContent value="MovementType" className={contentAnimation}>
+        Change your password here.
+      </TabsContent>
+      <TabsContent value="Unit" className={contentAnimation}>
+        Make changes to your account here.
+      </TabsContent>
+      <TabsContent value="Supplier" className={contentAnimation}>
+        Change your password here.
+      </TabsContent>
     </Tabs>
   );
 }
