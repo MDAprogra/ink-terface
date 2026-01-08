d9da35cb52951ba78a898ea9c226d921acd313cc
MatthiasInterfas <matthias@interfas.fr>
2026-01-08 14:40:39 +0100

    try to fix import error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+//Import
 import { GlobalScanListener } from '@/features/Global/GlobalScanListener';
 import { Providers } from '@/Providers';
 
