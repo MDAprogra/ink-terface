5b0dc412091adf50054393c2f8cbbf0ebc05ec73
MatthiasInterfas <matthias@interfas.fr>
2026-01-16 14:57:17 +0100

    fix: delete useless code

--- src/app/(with-layout)/(sidebar)/app/admin/users/page.tsx
+++ src/app/(with-layout)/(sidebar)/app/admin/users/page.tsx
@@ -1,9 +1,7 @@
-import { getAllRoles, getUsers } from '@/actions/administration/user';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { UsersTable } from '@/features/Administration/Users/Table';
 
 export default async function AdminUsersPage() {
-  const [users, roles] = await Promise.all([getUsers(), getAllRoles()]);
   return (
     <Card>
       <CardHeader>
